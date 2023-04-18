63472e466443f4895f5916423528fc70854298ee
BenjaminBrehier <benbrehier@gmail.com>
2023-04-18 20:10:22 +0200

    refactor: GameLogic name

--- src/main/java/fr/benjaminbrehier/_6quiprend/GameLogicController.java
+++ src/main/java/fr/benjaminbrehier/_6quiprend/GameLogicController.java
@@ -3,7 +3,7 @@ package fr.benjaminbrehier._6quiprend;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class GameLogicController {
     @FXML
     private Label welcomeText;
 
