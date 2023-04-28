e2262b44a0a388822693d198b56433ab4b8d054a
Syraps <115782856+Syraps@users.noreply.github.com>
2023-04-28 11:19:00 +0200

    initialisation et distribution gamelogic (cartes, plateau et joueurs)

--- src/main/java/fr/benjaminbrehier/_6quiprend/GameLogic.java
+++ src/main/java/fr/benjaminbrehier/_6quiprend/GameLogic.java
@@ -1,5 +1,8 @@
 package fr.benjaminbrehier._6quiprend;
 
+import fr.benjaminbrehier._6quiprend.Model.Card;
+import fr.benjaminbrehier._6quiprend.Model.IA;
+import fr.benjaminbrehier._6quiprend.Model.Player;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,11 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import static java.lang.Math.floor;
 
 public class GameLogic extends Application {
 
@@ -43,6 +51,54 @@ public class GameLogic extends Application {
             nbJoueurLbl.setText("Combien de joueurs ? : " + newValue.intValue()); // Met à jour le label avec la nouvelle valeur du slider
         });
         nbJoueurLbl.setText("Combien de joueurs ? : 6");
+        // Méthode générique pour randomiser une liste en Java à l'aide du mélange Fisher-Yates
+        ArrayList<Card> pioche = new ArrayList();
+        int NbJoueurs = 2;
+        Player joueur = new Player(null,null);
+        IA bot = new IA(null, null);
+        int i;
+        System.out.println(pioche.size());
+        for (i = 1; i<=104; i++){
+            double d = i / 10;
+            double c = i/5;
+            double o = i/11;
+
+            if (i == 55) {
+                pioche.add( new Card(i,7));
+            }else if (o == floor(o)) {
+               pioche.add( new Card(i,5));
+            }else if (d == floor(d)) {
+                pioche.add( new Card(i,3));
+            }else if (c == floor(c)) {
+                pioche.add( new Card(i,2));
+            }else{
+                pioche.add( new Card(i,1));
+            }
+        }System.out.println(pioche.size());
+        Collections.shuffle(pioche);
+        ArrayList<Card> ligne1 = new ArrayList();
+        ligne1.add(pioche.get(0));
+        pioche.remove(0);
+        ArrayList<Card> ligne2 = new ArrayList();
+        ligne2.add(pioche.get(0));
+        pioche.remove(0);
+        ArrayList<Card> ligne3 = new ArrayList();
+        ligne3.add(pioche.get(0));
+        pioche.remove(0);
+        ArrayList<Card> ligne4 = new ArrayList();
+        ligne4.add(pioche.get(0));
+        pioche.remove(0);
+        ArrayList<Card> playerHand = new ArrayList();
+        ArrayList<Card> botHand = new ArrayList();
+        for (i = 0; i<10; i++){
+            playerHand.add(pioche.get(0));
+            pioche.remove(0);
+            botHand.add(pioche.get(0));
+            pioche.remove(0);
+
+        }
+        joueur.setHand(playerHand);
+        bot.setHand(botHand);
 
     }
 
